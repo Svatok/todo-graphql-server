f04fba159200b922da5e88a1140ea02c9da6e2bf
Svatok <s_s_s@ua.fm>
2018-10-26 01:12:36 +0300

    Refactoring

--- app/graphql/application_schema.rb
+++ app/graphql/application_schema.rb
@@ -1,15 +1,5 @@
 class ApplicationSchema < GraphQL::Schema
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    raise GraphQL::ExecutionError, exception.message.truncate_words(3)
-  end
-
-  rescue_from Exceptions::AuthenticationError do
-    raise GraphQL::ExecutionError, 'You must be authenticated for this action'
-  end
-
-  rescue_from Pundit::NotAuthorizedError do
-    raise GraphQL::ExecutionError, 'You are not authoraized for this action'
-  end
+  include ::ErrorHandler
 
   mutation(MutationType)
   query(QueryType)
